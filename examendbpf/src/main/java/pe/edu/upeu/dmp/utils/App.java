ec43b7f198c569425e6ecacb0caf6be1744bacbd
Craftdoficial <Craftdoficial@gmail.com>
2022-06-25 20:17:12 -0500

    Segundo examen de programación

---
+++ examendbpf/src/main/java/pe/edu/upeu/dmp/utils/App.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.dmp.utils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello world" );
+    }
+}
